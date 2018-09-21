f0e24e3695620b31ec62d7c3c50d801b986b1916
Savannah Younts <savyounts@gmail.com>
2018-09-21 20:48:31 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,7 +4,7 @@ class Song < ActiveRecord::Base
   validates :artist_name, presence: true
 
   def release_year_true?
-    released == "true"
+    released == "false"
   end
 
   def not_unique_song?
