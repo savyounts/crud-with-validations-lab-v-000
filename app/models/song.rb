12c4a4fa9e3180a0a275c75ab1842e00d1314332
Savannah Younts <savyounts@gmail.com>
2018-09-21 19:03:24 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,9 @@
 class Song < ActiveRecord::Base
-  validates :title, presence: true 
+  validates :title, presence: true
   validates :released, inclusion: {in: %w(True False)}
   validate :release_year_required
   validates :release_year, if :release_year_true
-  
+
 
 
 Cannot be repeated by the same artist in the same year
@@ -16,4 +16,11 @@ Must not be blank
   def release_year_true
     released == True
   end
+
+  def unique_song
+    song = Song.find_by(title: title)
+    if song.artist_name != artist_name && song.release_year != release_year
+    end
+  end
+  
 end
