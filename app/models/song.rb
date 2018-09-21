921653c8a0f041d72e6dafdb255b4b9aab67abb3
Savannah Younts <savyounts@gmail.com>
2018-09-21 18:59:55 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,19 @@
 class Song < ActiveRecord::Base
+  validates :title, presence: true 
+  validates :released, inclusion: {in: %w(True False)}
+  validate :release_year_required
+  validates :release_year, if :release_year_true
+  
+
+
+Cannot be repeated by the same artist in the same year
+
+Must be less than or equal to the current year
+artist_name, a string
+Must not be blank
+
+
+  def release_year_true
+    released == True
+  end
 end
