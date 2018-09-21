43798c8d0f4ba917c6bbe6f68b5d45ce8e0b3d6d
Savannah Younts <savyounts@gmail.com>
2018-09-21 21:11:59 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,18 +7,10 @@ class ReleaseYearValidator < ActiveModel::Validator
 end
 
 class Song < ActiveRecord::Base
-  validates :title, presence: true, uniqueness: false, unless: :not_unique_song?
+  validates :title, presence: true, uniqueness: {scope: :artist_name, :release_year}
   validates_with ReleaseYearValidator
   validates :artist_name, presence: true
   validates :release_year, numericality: {less_than_or_equal_to: DateTime.now.year}
-
-  # def release_year_false?
-  #   released == "false"
-  # end
-
-  def not_unique_song?
-    Song.find_by(title: title, release_year: release_year, artist_name: artist_name)
-  end
 end
 
 #
