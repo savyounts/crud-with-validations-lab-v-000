d5e122a6b004196cf5fca06f65b858b595c5c571
Savannah Younts <savyounts@gmail.com>
2018-09-21 20:12:45 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -36,7 +36,7 @@ class SongsController < ApplicationController
   end
 
   def destroy
-    @song = Song.find(params[:id]).destroy
+    Song.find(params[:id]).destroy
     redirect_to songs_path
   end
 
