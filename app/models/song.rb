794688cb87462333023915a9c468efc78254530a
Savannah Younts <savyounts@gmail.com>
2018-09-21 20:56:18 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class ReleaseYearValidator < ActiveModel::Validator
   def validate(record)
-    if record.released == "true" && record.release_year.blank?
+    if record.released == "true" && record.release_year.nil?
       record.errors[:base] << "Must fill in release year"
     end
   end
