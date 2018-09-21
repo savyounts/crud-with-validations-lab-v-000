fd0909864d941ea90e2bdf01710620dc6f1619ba
Savannah Younts <savyounts@gmail.com>
2018-09-21 19:10:18 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,22 +7,25 @@ class SongsController < ApplicationController
   end
 
   def create
-  end 
-
-  def edit 
-  end 
-
-  def update 
   end
 
-  def show 
+  def edit
+  end
+
+  def update
+  end
+
+  def show
   end
 
   def destroy
   end
 
   private
-    def set_song 
+
+    def song_params 
+      params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
+    def set_song
       @song = Song.find(params[:id])
     end
 end
