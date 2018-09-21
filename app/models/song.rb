35cd59e0941f20d9e91431c837620dea8c1bf884
Savannah Younts <savyounts@gmail.com>
2018-09-21 21:01:52 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -10,7 +10,7 @@ class Song < ActiveRecord::Base
   validates :title, presence: true, uniqueness: false, unless: :not_unique_song?
   validates_with ReleaseYearValidator
   validates :artist_name, presence: true
-  validates :release_year, numericality: {less_than_or_equal_to: 2018}
+  validates :release_year, numericality: {less_than_or_equal_to: DateTime.now.year}
 
   # def release_year_false?
   #   released == "false"
