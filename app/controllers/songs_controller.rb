8831c965bb27b168cc84c7286a90a869593b58de
Savannah Younts <savyounts@gmail.com>
2018-09-21 19:13:21 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,33 +1,47 @@
 class SongsController < ApplicationController
   before_action :set_song, [:edit, :update, :show, :destroy]
-  
+
   def index
-    @
+    @songs = Song.all
   end
 
   def new
+    @song = Song.new
   end
 
   def create
+    if @song = Song.new(song_params)
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @song.update
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
   end
 
   def show
   end
 
   def destroy
+    @song.destroy
+    redirect_to :index
   end
 
   private
 
     def song_params
       params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
-    end 
+    end
 
     def set_song
       @song = Song.find(params[:id])
