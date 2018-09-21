794cef24bd5c50f9b746d4c71daaaa262b05b45e
Savannah Younts <savyounts@gmail.com>
2018-09-21 19:09:27 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,28 @@
 class SongsController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end 
+
+  def edit 
+  end 
+
+  def update 
+  end
+
+  def show 
+  end
+
+  def destroy
+  end
+
+  private
+    def set_song 
+      @song = Song.find(params[:id])
+    end
 end
