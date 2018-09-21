510c864b67bbc947e3d8739c728a197e32ee6ff2
Savannah Younts <savyounts@gmail.com>
2018-09-21 20:18:35 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,5 @@
 class Song < ActiveRecord::Base
-  validates :title, presence: true, uniqueness: true
+  validates :title, presence: true, uniqueness: true, unless: :unique_song?
   validates :release_year, presence: true, unless: :release_year_true?
   validates :artist_name, presence: true
 
@@ -7,10 +7,11 @@ class Song < ActiveRecord::Base
     released == "True"
   end
 
+  def unique_song?
+    song = Song.find_by(title: title)
+    song.artist_name != artist_name && song.release_year != release_year
+  end
 end
 
 #
-# def unique_song?
-#   song = Song.find_by(title: title)
-#   song.artist_name != artist_name && song.release_year != release_year
-# end
+# 
